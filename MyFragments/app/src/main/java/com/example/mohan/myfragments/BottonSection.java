c24aeb32e2f35c1e869517305b37d0ab6ffdf3c7
MOHAN <singhmohan333@gmail.com>
2017-07-07 11:23:31 +0530

    new

---
+++ MyFragments/app/src/main/java/com/example/mohan/myfragments/BottonSection.java
@@ -0,0 +1,19 @@
+package com.example.mohan.myfragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BottonSection extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.bottom_section, container, false);
+        return view;
+    }
+
+}
---
+++ MyFragments/app/src/main/java/com/example/mohan/myfragments/TopSection.java
@@ -0,0 +1,28 @@
+package com.example.mohan.myfragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.AppLaunchChecker;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TopSection extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_section, container, false);
+        return view;
+
+    }
+
+
+}
+
+
+
